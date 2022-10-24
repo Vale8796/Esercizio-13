3d75aa75ade8adb76d2657de8e6dc36fdf14e733
Valeria Mancuso <110185990+Vale8796@users.noreply.github.com>
2022-10-24 17:16:11 +0200

    Update script1.js

--- Start/assets/Js/script1.js
+++ Start/assets/Js/script1.js
@@ -1,7 +1,7 @@
 var data = new Date();
 let ora = data.getHours();
 
-if (ora < 12) {
+if (ora > 5 && ora < 12) {
     document.getElementById('saluto').innerHTML = `Buongiorno! Sono le ${data.getHours()}:${data.getMinutes()}.`;
 } else if (ora > 12 && ora < 18) {
     document.getElementById('saluto').innerHTML = `Buon Pomeriggio! Sono le ${data.getHours()}:${data.getMinutes()}.`;
